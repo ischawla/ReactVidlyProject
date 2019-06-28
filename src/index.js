01028a88fcfd624bad92715cc6e39e9fa26f43b0
ischawla <inderjeet.mca@gmail.com>
2019-06-28 06:08:00 -0400

    env related files for deployment added
    env related files for deployment added

--- src/index.js
+++ src/index.js
@@ -7,6 +7,13 @@ import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
 import { BrowserRouter } from "react-router-dom";
 
+//VARIABLE SHOULD ALWAYS START WITH 'REACT_APP'
+//When we run the application using -
+// npm start : Development Env variable will be picked from .env.devlopment
+// npm run build: Production Env variable will be invoked from '.env.production'
+// npm test : Test Env variable will be invoked from '.env.test'
+console.log(process.env);
+
 ReactDOM.render(
   <BrowserRouter>
     <App />
